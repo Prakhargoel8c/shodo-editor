bbefb0b7a2a6dfbd97510f2f67029483a9b7bcd1
Prakhar Goel <rockingprakhar.987@outlook.com>
2023-09-09 06:51:03 +0530

    Added Github Icon

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,17 @@
+import { Github } from '@/ui/icons';
 import Image from 'next/image';
+import Link from 'next/link';
 
 export default function Home() {
-  return <main className="flex min-h-screen flex-col items-center justify-between p-24"></main>;
+  return (
+    <div className="flex min-h-screen flex-col items-center sm:px-5 sm:pt-[calc(20vh)]">
+      <Link
+        href="/github"
+        target="_blank"
+        className="absolute bottom-5 left-5 z-10 max-h-fit rounded-lg p-2 transition-colors duration-200 hover:bg-stone-100 sm:bottom-auto sm:top-5"
+      >
+        <Github />
+      </Link>
+    </div>
+  );
 }

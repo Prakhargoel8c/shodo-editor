8b44f4194d60f7b58cb020810ee1d837990aeff3
Prakhar Goel <rockingprakhar.987@outlook.com>
2023-09-11 12:09:27 +0530

    Renamed Project To Shodo AI

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,14 +5,12 @@ import Providers from './providers';
 
 const inter = Inter({ subsets: ['latin'] });
 
-const title = 'Notion Clone';
-const description = 'A Notion-style WYSIWYG editor with AI-powered autocompletions. Built with Tiptap, OpenAI, and Vercel AI SDK.';
-
+const title = 'Shodo AI';
+const description = 'Shodo AI is a AI first text editor to help you move faster and write better.';
 export const metadata: Metadata = {
   title,
   description,
   openGraph: { title, description },
-  twitter: { card: 'summary_large_image', title, description, creator: '@Prakhar_Go_el' },
   themeColor: '#ffffff',
 };
 

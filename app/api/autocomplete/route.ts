f52436be8197831a993ca9efa225aa0fa6054d8e
Prakhar Goel <rockingprakhar.987@outlook.com>
2023-09-13 09:10:04 +0530

    Improved Prompt

--- app/api/autocomplete/route.ts
+++ app/api/autocomplete/route.ts
@@ -14,10 +14,14 @@ export async function POST(req: Request) {
     n: 1,
     frequency_penalty: 0,
     presence_penalty: 0,
+    temperature: 0.7,
     messages: [
       {
         role: 'system',
-        content: 'You are an AI writing assistant that continues existing text based on context from prior text.',
+        content:
+          'You are an AI writing assistant that continues existing text based on context from prior text.' +
+          'Give more weight/priority to the later characters than the beginning ones. ' +
+          'Limit your response to no more than 200 characters, but make sure to construct complete sentences.',
       },
       {
         role: 'user',

8e61f02ed02beebf026af98a679c7d97d86a5dec
Prakhar Goel <rockingprakhar.987@outlook.com>
2023-09-14 10:37:45 +0530

    Save the content to local storage to load next time

--- ui/Editor.tsx
+++ ui/Editor.tsx
@@ -11,12 +11,13 @@ export default function Editor() {
     setSaveStatus('Saving...');
     // Simulate a delay in saving
     setTimeout(() => setSaveStatus('Saved'), 500);
+    localStorage.setItem('content', JSON.stringify(editor.getJSON()));
   }, 1000);
 
   return (
     <div className="relative w-full max-w-screen-lg">
       <div className="absolute right-5 top-5 z-10 mb-5 rounded-lg bg-stone-100 px-2 py-1 text-sm text-stone-400">{saveStatus}</div>
-      <RichTextEditor completionApi="/api/autocomplete" />
+      <RichTextEditor completionApi="/api/autocomplete" onUpdate={debouncedSave} defaultValue={JSON.parse(localStorage.getItem('content') ?? '')} />
     </div>
   );
 }
